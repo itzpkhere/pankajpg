af92406b0a4c04fa1e98f99d72b2f78035bc4380
itzpkhere <74782373+itzpkhere@users.noreply.github.com>
2024-08-28 16:42:20 +0530

    Update Manish.java

--- kumar/src/banti/Manish.java
+++ kumar/src/banti/Manish.java
@@ -4,5 +4,6 @@ public class Manish {
 public static void main(String []args)
 {
 	System.out.println("i am a prime minister of india");
+	System.out.println("i am a chief minister of karnataka")
 }
 }
